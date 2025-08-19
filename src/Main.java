306fe2987b774140ac3f1fec1cd332b42b72914a
Olga Zotova <olgazotova1976@yandex.ru>
2025-08-19 11:18:32 +0300

    Remove src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,22 @@
 public class Main {
+
+    public static void main(String[] args) {
+        int initialBalance = 100; // Начальный счет клиента
+        int topUpAmount = 1100; // Сумма пополнения счета
+
+        // Переменная для хранения суммы бонусов
+        int bonus = 0;
+
+        // Условный оператор проверки, превышает ли сумма пополнения 1000 рублей
+        if (topUpAmount > 1000) {
+            bonus = topUpAmount / 100; // Бонус - 1 рубль за каждые 100 рублей
+        }
+
+        int finalBalance = initialBalance + topUpAmount + bonus;
+
+        System.out.println("Итоговая сумма на счету:" + finalBalance + " рублей.");
+        System.out.println("Количество бонусных рублей:" + bonus + " рублей.");
+
+
+    }
 }
